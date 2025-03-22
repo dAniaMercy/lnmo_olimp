13e428b38979a5ff7bb87dc8680f91662c601833
Daniil <dan.b3liakov@yandex.ru>
2025-03-22 11:11:50 +0300

    22.03.25 0.0.4

--- README.md
+++ README.md
@@ -1,9 +1,4 @@
 
-#      LL             OOOOOOOO      RRRRRRRRR       EEEEEEEEEE     NNNN     NN
-#      LL            OO      OO     RR      RR      EE             NN NN    NN
-#      LL           OO        OO    RR       RR     EE             NN  NN   NN
-#      LL          OO          OO   RR      RR      EEEEEEE        NN   NN  NN
-#      LL           OO        OO    RRRRRRR         EEEEEEE        NN    NN NN
-#      LL            OO      OO     RR    RR        EE             NN     NNNN
-#      LLLLLLLLL      OOOOOOOO      RR     RR       EEEEEEEEEE     NN      NNN
+# LOREN_OLIMP
 
+* ## Задачи с Main c https://acmp.ru/index.asp?main=tasks 
--- src/problemC.java
+++ src/problemC.java
@@ -1,2 +1,73 @@
+import java.util.*;
+
 public class problemC {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            int L = scanner.nextInt();
+
+            List<int[]> obstacles = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                int l = scanner.nextInt();
+                int r = scanner.nextInt();
+                obstacles.add(new int[]{l, r});
+            }
+
+            List<int[]> powerUps = new ArrayList<>();
+            for (int i = 0; i < m; i++) {
+                int x = scanner.nextInt();
+                int v = scanner.nextInt();
+                powerUps.add(new int[]{x, v});
+            }
+
+            System.out.println(minPowerUps(n, m, L, obstacles, powerUps));
+        }
+
+        scanner.close();
+    }
+
+    private static int minPowerUps(int n, int m, int L, List<int[]> obstacles, List<int[]> powerUps) {
+        int pos = 1, jump = 1, powerUpCount = 0, index = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int[] obstacle : obstacles) {
+            int l = obstacle[0], r = obstacle[1];
+
+            while (pos < l) {
+                while (index < m && powerUps.get(index)[0] <= pos) {
+                    pq.offer(powerUps.get(index)[1]);
+                    index++;
+                }
+
+                if (pos + jump >= l) break;
+
+                if (pq.isEmpty()) return -1;
+
+                jump += pq.poll();
+                powerUpCount++;
+            }
+
+            pos = r + 1;
+        }
+
+        while (pos < L) {
+            while (index < m && powerUps.get(index)[0] <= pos) {
+                pq.offer(powerUps.get(index)[1]);
+                index++;
+            }
+
+            if (pos + jump >= L) return powerUpCount;
+
+            if (pq.isEmpty()) return -1;
+
+            jump += pq.poll();
+            powerUpCount++;
+        }
+
+        return powerUpCount;
+    }
 }
